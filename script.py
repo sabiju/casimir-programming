b8cfcdf42c509517cbb3cc208da0d9b9f1eda298
sabiju <s.valiyavalappil@tudelft.nl>
2021-01-25 13:53:00 +0100

    added test and script files

---
+++ script.py
@@ -0,0 +1,5 @@
+print ("sth")
+import test
+r = 1
+circumference = test.circumference(r)
+surf_area = test.surface_area(r)
--- test.py
+++ test.py
@@ -1,1 +1,6 @@
+import numpy as np 
 print ("hello world")
+def circumference(r):
+	return np.pi*2*r
+def surface_area (r):
+	return np.pi*r**2
